70a5607e59c72cdf87623afd9cdadc8c2661b31d
Rostyslav <rstq.brh@gmail.com>
2020-04-22 13:53:42 +0300

    Fixed img

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -9,7 +9,7 @@ public class TestResult {
          */
         Exercise ex = new Exercise(97,26);
         String enc = ex.encrypt("poltava","ppc");
-        System.out.println("Russian text - " + enc);
+        System.out.println("English text - " + enc);
         String dec = ex.decrypt(enc,"ppc");
         System.out.println("Decrypted text - " + dec);
         /**
@@ -17,7 +17,7 @@ public class TestResult {
          */
         Exercise exercise = new Exercise(1072, 33);
         enc = exercise.encrypt("полтава", "ппк");
-        System.out.println("English text - " + enc);
+        System.out.println("Russian text - " + enc);
         dec = exercise.decrypt(enc, "ппк");
         System.out.println("Decrypted text - " + dec);
     }
